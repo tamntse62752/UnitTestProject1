3fe506b754e54df4169602438f210b732e2f75c7
hungdang45 <46567162+hungdang45@users.noreply.github.com>
2019-06-27 17:14:39 +0700

    Add project files.

---
+++ LoginTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Winium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    
+    public class LoginTest
+    {
+        //private const string PosAppId = @"E:\Wisky\NewPos\POS\POS\bin\Debug\SkyPOS.exe";
+
+        private const string PosAppId = @"C:\Program Files (x86)\SkyPOS\SkyPOS.exe";
+        
+
+        protected static WiniumDriver driver;
+        public static void LoadApp(TestContext context)
+        {
+            //var service = WiniumDriverService.CreateDesktopService(@"C:\Users\bom4e\Downloads\Winium.Desktop.Driver");
+            var service = WiniumDriverService.CreateDesktopService(@"C:\Users\HungDang\Downloads\Winium.Desktop.Driver");
+        
+
+         DesktopOptions option = new DesktopOptions
+            {
+                ApplicationPath = PosAppId
+            };
+            driver = new WiniumDriver(service, option, TimeSpan.FromSeconds(60));
+        }
+        
+    }
+}
---
+++ UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Winium;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest : LoginTest
+    {
+        //private const string ClassName = "WindowsForms10.EDIT.app.0.205b375_r9_ad1";
+        private const string ClassName = "WindowsForms10.Window.8.app.0.205b375_r9_ad1";
+       
+        public IWebElement btnLogin;
+
+               
+        [TestMethod]
+        public void UpdateProduct()
+        {
+            //ReadOnlyCollection<IWebElement> UpdateBtn = driver.FindElementsById("btnConfig");
+            driver.FindElementById("btnConfig").Click();
+            //driver.FindElementByName("").Click();
+            Thread.Sleep(2000);
+            driver.FindElementById("btnUpdate").Click();
+            Thread.Sleep(2000);
+            driver.FindElementById("btnUpdateProduct").Click();
+            
+        }
+
+        [TestMethod]
+        public void UpdateCategory()
+        {
+            //ReadOnlyCollection<IWebElement> UpdateBtn = driver.FindElementsById("btnConfig");
+            driver.FindElementById("btnConfig").Click();
+            //driver.FindElementByName("").Click();
+            Thread.Sleep(2000);
+            driver.FindElementById("btnUpdate").Click();
+            Thread.Sleep(2000);
+            driver.FindElementById("btnUpdateCategory").Click();
+
+        }
+        
+        [TestMethod]
+      
+        [ClassInitialize]
+        public static void loadForm(TestContext context)
+        {
+            LoadApp(context);
+            IWebElement btnConfig = driver.FindElementById("btnConfig");
+ 
+            //IWebElement txtUsername = driver.FindElementById("txtUsername");
+            //IWebElement txtPassword = driver.FindElementById("txtPassword");
+            //Assert.IsNotNull(btnConfig);
+        }
+    }
+}
